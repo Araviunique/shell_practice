6db2498aff5ea71a2143a65072559c4abe1c8b07
Aravind <saiaravinduppala@gmail.com>
2026-09-06 18:20:25 +0530

    tenth commit

---
+++ 03_varaible_dif.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+person1=$1
+person2=$2
+echo "$person1: Hi $person2 i will blast you"
+echo "$person2: Hehe,Nice joke.go and play child games"
+echo "$person1: $person2.I am serious"
+echo "$person2: $person1.I too serious"
