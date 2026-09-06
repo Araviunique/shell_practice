a8f86920099e7a79ff95b38aa4798a9560513f7c
Aravind <saiaravinduppala@gmail.com>
2026-09-06 18:15:36 +0530

    tenth commit

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 person1=putin
-person3=zelenski
+person2=zelenski
 echo "$person1: Hi $person2 i will blast you"
 echo "$person2: Hehe,Nice joke.go and play child games"
 echo "$person1: $person2.I am serious"
